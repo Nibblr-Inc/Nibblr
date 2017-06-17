89f6364ec49b12b56b0e3b53dd69146071f46738
Michael Mahon <mikemahonx@utexas.edu>
2017-06-17 15:19:18 -0500

    make dummy data

---
+++ dummy-data.js
@@ -0,0 +1,11 @@
+ var dummyData = [{
+  eventID: 1,
+  eventName: 'this is an event'
+  datatime: '2017:10:07 06:06:06',
+  location: 1, // api location id
+  creator: 1,
+  description: 'this is a description',
+  rsvps: [{username: 'bob'}, {username: 'bill'}]
+}]
+
+exports.dummyData = dummyData;
