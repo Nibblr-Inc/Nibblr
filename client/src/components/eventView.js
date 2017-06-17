5741909c8a9d1171bbe381292aa99dbbd6ee60dc
Ada323 <mag242@gmail.com>
2017-06-17 17:07:52 -0500

    Add skeletons for menu, createEvent, eventView, and app

---
+++ client/src/components/eventView.js
@@ -0,0 +1,9 @@
+angular.module('nibblr')
+
+.directive('eventView', function() {
+  return {
+    scope: {},
+    controller: function() {},
+    templateUrl: 'client/src/templates/eventView.html'
+  }
+})
