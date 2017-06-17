af8d72e9cb97ebdbb30e6319e03c88f7de2e39f9
Nate Green <nate.exe@gmail.com>
2017-06-17 17:05:46 -0500

    Add basic rendering for front end components

--- dummy-data.js
+++ dummy-data.js
@@ -1,7 +1,7 @@
  var dummyData = [{
   eventID: 1,
   eventName: 'this is an event'
-  datatime: '2017:10:07 06:06:06',
+  datetime: '2017:10:07 06:06:06',
   location: 1, // api location id
   creator: 1,
   description: 'this is a description',
