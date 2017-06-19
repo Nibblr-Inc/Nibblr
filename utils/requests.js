5fc730e9fa029b55ca1ab802beaad7cda35595e4
Michael Mahon <mikemahonx@utexas.edu>
2017-06-19 15:25:37 -0500

    adds place details request and modifies schema

--- utils/requests.js
+++ utils/requests.js
@@ -1,8 +1,10 @@
 var https = require('https');
+var google_api_key = require('../config.js').google_api_key;
 
+console.log('key: ', google_api_key);
 
 exports.nearbySearch = function(req, res) {
-  var key = window.Google_API_Key;
+  var key = google_api_key;
   var location = encodeURIComponent('30.2671500,-97.7430600')
   var radius = 16000;
   var sensor = false;
@@ -31,10 +33,10 @@ exports.nearbySearch = function(req, res) {
 
 
 exports.placeDetails = function(req, res) {
-  var key = window.Google_API_Key;
+  var key = google_api_key;
   var placeid = req.query.placeid;
 
-  var url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?" + "key=" + key + "&placeid=" + placeid;
+  var url = "https://maps.googleapis.com/maps/api/place/details/json?" + "key=" + key + "&placeid=" + placeid;
     console.log(url);
   https.get(url, function(response) {
     var body ='';
@@ -44,14 +46,12 @@ exports.placeDetails = function(req, res) {
 
     response.on('end', function() {
       var place = JSON.parse(body);
-      // var locations = places.results;
+      var location = place.result;
 
-      console.log('place: ', place)
-      // res.json(locations)
+      console.log('location: ', location)
+      res.json(location)
     });
   }).on('error', function(e) {
     console.log("Got error: " + e.message);
   });
 };
-
-exports.placeDetails({query: {placeid: 'ChIJ6WLDCS00W4YR_w3C9RY4Fdc'}});
