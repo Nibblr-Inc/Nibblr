f8a9d9ee8bb927b5232b3a40a630f1893bd7d380
Ada323 <mag242@gmail.com>
2017-06-21 13:19:17 -0500

    Work on router and sessions

--- server/router.js
+++ server/router.js
@@ -5,10 +5,29 @@ var controller = require('./controllers/index.js');
 
 var router = express.Router();
 
+//Middleware function to check login status
+router.use('/create', function(req, res, next){
+  if(req.session.loggedIn) {
+    next();
+  } else {
+    res.redirect('/login');
+  }
+});
+
+router.use('/rsvp', function(req, res, next){
+  if(req.session.loggedIn) {
+    next();
+  } else {
+    console.log('redirect from rsvp')
+    res.redirect('/login');
+  }
+});
+
 //API Requests
 
 router.route('/searchRestaurants')
   .get(function(req, res) {
+    //make API call
     requests.nearbySearch(req, res);
   })
 
@@ -16,55 +35,79 @@ router.route('/searchRestaurants')
 
 router.route('/list')
   .get(function(req, res) {
-    // insert helper function(s) for event details get requests
-    res.send('list test')
+    console.log('got it')
+    // fetch data for all events
+    controller.events.get(req, res);
   });
 
 router.route('/create')
   .get(function(req, res) {
-    // insert helper function(s) for displaying a newly-added event here
-    res.send('create test')
+    // fetch event list once new event has been added
+    controller.events.get(req, res);
   })
   .post(function(req, res) {
-    // insert helper function(s) for saving a new event in the database
+    // add event to database
+    controller.events.post(req, res);
   })
+
+  router.route('/rsvp')
+    .get(function(req, res) {
+      controller.rsvp.getByEvent(req, res);
+    })
+    .post(function(req, res) {
+      controller.rsvp.post(req, res);
+    })
+    .delete(function(req, res) {
+      controller.rsvp.delete(req, res);
+    })
 
 // Authentication - add routes here
 
 router.route('/signup')
   .get(function(req, res) {
     // render signup page
-    res.send('signup test')
+    res.send('signup test - get')
   })
   .post(function(req, res) {
-    // check if username exists
-    // if not, hash password & save username and hashed password to database
-    // create session using express-session
-    // once complete, redirect to home page
-    // if username already exists, display error message telling user to choose another
+    var exists = controller.users.get(req, res);
+    if (!exists) {
+      controller.users.post(req, res);
+      req.session.loggedIn = true;
+      res.redirect('/');
+    } else {
+      res.send('username not available, please choose another');
+    }
   })
 
 router.route('/login')
   .get(function(req, res) {
     // render login page
-    res.send('login test')
+    res.send('login test - get')
   })
   .post(function(req, res) {
+    console.log('req.session', req.session)
     controller.login.get(req, res, function(userData) {
-      if (userData) {/* regenerate user session with userData */}
+      if (userData) {
+        console.log('userData', userData)
+        req.session.regenerate(function() {
+          req.session.user_id = id;
+          req.session.user = username;
+          req.session.loggedIn = true;
+          console.log('login success')
+          res.redirect('/');
+        });
+      } else {
+        console.log('no userData')
+        res.send('Password does not match username - please try again')
+      }
     });
-    //compare hashed passwords
-    //if they match, regenerate session
-    //save userId on session
-    //redirect to home page
-    //if hashed passwords don't match, display error message asking user to try again
   })
 
 router.route('/logout')
   .get(function(req, res) {
-    //destroy session
-    //redirect to home page
-    res.send('logout test')
+    req.session.destroy();
+    console.log('logged out', req.session) //should be undefined
+    res.redirect('/');
   })
 
 
