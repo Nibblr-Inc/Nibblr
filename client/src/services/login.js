389fb3d1f97b54fac75358f556640d186e2493df
Michael Mahon <mikemahonx@utexas.edu>
2017-06-22 14:44:28 -0500

    completes login and signup services

--- client/src/services/login.js
+++ client/src/services/login.js
@@ -1,20 +1,13 @@
 .service('login', function($http){
   // TODO
 
-  var makeQueryString = function(url, params) {
-    var paramsArray = []
-    for (p in params) {
-      paramsArray.push(p + '=' + params[p]);
-    }
-    return url + paramsArray.join('&');
-  }
-
-  this.login = function(query, callback) {
+  this.login = function(body, callback) {
     $http({
-    // query should be obj with keys: username, password
+    // body should be obj with keys: username, password
     url: makeQueryString('http://localhost:3000/login?', query),
-    method: 'GET',
+    method: 'POST',
     dataType: 'json',
+    data: body,
     }).then(function successCallback(response) {
       if (callback) {
         callback(response);
