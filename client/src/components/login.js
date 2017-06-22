1e0805f3325a6c6622f7295ef25b5330d8b3f125
Ada323 <mag242@gmail.com>
2017-06-22 12:17:48 -0500

    Work on login form and button

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -1,9 +1,39 @@
 angular.module('nibblr')
 
-.directive('menu', function() {
+.directive('login', function() {
   return {
     scope: {},
-    controller: function() {},
+    controller: function() {
+      var $ = angular.element;
+      var overlay = $("#overlay"),
+              fab = $(".fab"),
+           cancel = $("#cancel"),
+           submit = $("#submit");
+
+      //fab click
+      fab.on('click', openFAB);
+      overlay.on('click', closeFAB);
+      cancel.on('click', closeFAB);
+
+      function openFAB(event) {
+        if (event) event.preventDefault();
+        fab.addClass('active');
+        overlay.addClass('dark-overlay');
+
+      }
+
+      function closeFAB(event) {
+        if (event) {
+          event.preventDefault();
+          event.stopImmediatePropagation();
+        }
+
+        fab.removeClass('active');
+        // fab.removeClass('mdl-navigation__link');
+        overlay.removeClass('dark-overlay');
+
+      }
+    },
     templateUrl: 'client/src/templates/login.html'
   }
 
