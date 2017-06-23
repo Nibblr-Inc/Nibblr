2a40d3a7c4c769ccbf11fcfb200b363679256210
Nate Green <nate.exe@gmail.com>
2017-06-23 12:02:18 -0500

    Add time picker to create event

--- client/src/createEvent/components/app.js
+++ client/src/createEvent/components/app.js
@@ -1,2 +1,1 @@
-
-angular.module('createEvent', [])
+angular.module('createEvent', ['moment-picker'])
---
+++ client/src/createEvent/components/dateTime.js
@@ -0,0 +1,14 @@
+angular.module('createEvent')
+
+.directive('dateTime', function() {
+  return {
+    scope: {
+    },
+    controller: function() {
+    },
+    controllerAs: 'ctrl',
+    bindToController: true,
+    templateUrl: 'client/src/createEvent/templates/dateTime.html'
+  }
+
+});
