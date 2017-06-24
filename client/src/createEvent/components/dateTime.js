b962a81fa6e7a6e6f3f10da34d6b94e2424f2b33
Nate Green <nate.exe@gmail.com>
2017-06-24 12:33:15 -0500

    Fix date format

--- client/src/createEvent/components/dateTime.js
+++ client/src/createEvent/components/dateTime.js
@@ -8,10 +8,11 @@ angular.module('createEvent')
 
       this.handleDateChange = function(item) {
         window.eventData.event_time = this.formatDate(item._d);
-        console.log('item',item);
+        console.log('item',this.formatDate(item._d));
       };
 
       this.formatDate = function(date) {
+        console.log('date',date)
         return date.toISOString().replace('T', ' ').slice(0, -5);
       }
     },
