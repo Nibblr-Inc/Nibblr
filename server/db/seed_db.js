8651dd9ef5b2464c65cad012a308f7ac754f87eb
Michael Mahon <mikemahonx@utexas.edu>
2017-06-24 15:36:16 -0500

    adds photo_url to seed data

--- server/db/seed_db.js
+++ server/db/seed_db.js
@@ -5,8 +5,8 @@ controller.users.post({body: {username: 'nate', password: '123'}})
 controller.users.post({body: {username: 'molly', password: 'abc123'}})
 
 setTimeout(function() {
-  controller.events.post({body: {name: 'first event', event_time: '2017-07-04 12:30:00', location: 'Home Slice', google_place_id: 'madeupid', description: 'this is a test', address: '304 W Mary St', category: 'pizza', photo_url: 'asdfasd'}, session: {user_id: 1}})
-  controller.events.post({body: {name: 'second event', event_time: '2017-07-05 4:30:00', location: 'Lazurus', google_place_id: 'madeupid2', description: 'this is a test2', address: '304 E Mary St', category: 'mexican', photo_url: 'adsfafds'}, session: {user_id: 2}})
+  controller.events.post({body: {name: 'first event', event_time: '2017-07-04 12:30:00', location: 'Home Slice', google_place_id: 'madeupid', description: 'this is a test', address: '304 W Mary St', category: 'pizza', photo_url: 'https://lh3.googleusercontent.com/p/AF1QipPloRke1QOxtAbYX27sHiVtUV2uOHS4dcWPDkYA=s1600-w512-h1363'}, session: {user_id: 1}})
+  controller.events.post({body: {name: 'second event', event_time: '2017-07-05 4:30:00', location: 'Lazurus', google_place_id: 'madeupid2', description: 'this is a test2', address: '304 E Mary St', category: 'mexican', photo_url: 'https://lh3.googleusercontent.com/p/AF1QipPloRke1QOxtAbYX27sHiVtUV2uOHS4dcWPDkYA=s1600-w512-h1363'}, session: {user_id: 2}})
 }, 100);
 
 setTimeout(function() {
