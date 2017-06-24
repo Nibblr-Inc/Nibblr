1b8147319629ddbdb1e6b117b73e84c7066ee48f
Ada323 <mag242@gmail.com>
2017-06-24 17:40:34 -0500

    Make minor change to fix rsvp issue

--- client/src/home/components/eventView.js
+++ client/src/home/components/eventView.js
@@ -9,7 +9,7 @@ angular.module('nibblr')
     controller: function(rsvpRequests, eventsRequests) {
       this.rsvpClick = (id) => {
         console.log('in rsvp click function')
-        rsvpRequests.postRSVP({event_id: id, rsvp_user_id: rsvp_user_id, creatorID: creatorID}, function(data){
+        rsvpRequests.postRSVP({event_id: id}, function(data){
           if (data.status === 201) {
             alert('Success!')
             eventsRequests.getEvents({event_id: id}, function({data}) {
@@ -17,7 +17,7 @@ angular.module('nibblr')
               console.log('event data: ', this.event)
               this.event.rsvp_usernames = data[0].rsvp_usernames;
             }.bind(this))
-            
+
           } else if (data.status === 400) {
             alert("You're already RSVP'd for this event")
 
